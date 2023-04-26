b92271ce131a278fb3124ad5e76179db0980db1b
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2023-04-26 19:35:33 +0600

    added readme file

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { CookiesProvider } from "react-cookie";
 
 import App from "./App";
 import "./index.css";
@@ -22,9 +21,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <GlobalContext>
-          <CookiesProvider>
-            <App />
-          </CookiesProvider>
+          <App />
         </GlobalContext>
       </BrowserRouter>
       <ReactQueryDevtools />
