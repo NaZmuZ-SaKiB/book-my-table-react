5b577cb988c0dd9b5da665067233932cc295388d
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-06-10 13:17:08 +0600

    removed jwt from config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,4 @@
 export default {
   node_env: import.meta.env.NODE_ENV || "development",
   api_base_url: import.meta.env.VITE_BASE_API_URL,
-  jwt_secret: import.meta.env.VITE_JWT_TOKEN_SECRET,
 };
